0439967dbde863f580e72884bcc3d85a453b9a69
Reuben Dawe <reubendawemarketing@gmail.com>
2024-12-21 18:19:44 +0000

    Created Arithmetic segment

--- arithmetic.php
+++ arithmetic.php
@@ -1,3 +1,45 @@
+<?php
+
+
+// -------------------- Arithmetic operators
+// + - * / ** %
+
+$x = 10;
+$y = 2;
+$z = null;
+
+// $z = $x + $y;
+// $z = $x - $y;
+// $z = $x * $y;
+// $z = $x / $y;
+// $z = $x ** $y;
+$z = $x % $y;
+echo $z;
+echo"<br>";
+
+// -------------------- Increment/decrement operators
+// ++, --
+
+$counter = 0;
+
+// $counter = $counter + 1;
+// $counter++;
+// $counter--;
+// $counter+=2;
+$counter-=2;
+echo $counter;
+
+// -------------------- Operator Precedence
+// In order left from left to right, parentheses first 
+// () 
+// **
+// * / %
+// + -
+
+$total = 1 + 2 - 3 * 4 / 5 ** 6;
+echo $total;
+
+?>
 <!-- HTML -->
 
 <!DOCTYPE html>
