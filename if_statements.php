7bee796215c5c056a4deb3eab6a0c269ea8d508c
Reuben Dawe <reubendawemarketing@gmail.com>
2024-12-29 18:27:44 +0000

    If_Statements page complete

--- if_statements.php
+++ if_statements.php
@@ -22,3 +22,54 @@
 
 </body>
 </html>
+
+<?php
+
+// If statement - If some condition is true, do something
+//                If condition is false, don't do it
+
+// $age = 101;
+
+// if($age >= 100) {
+//     echo "You are too old to enter this site";
+// }
+// elseif ($age >= 18){
+//     echo "You may enter this site";
+// }
+// elseif($age <= 0) {
+//     echo "That wasn't a valid age";
+// }
+// else{
+//     echo "You must be 18+ enter";
+// }
+
+
+
+// $adult = true;
+
+// if($adult){
+//     echo"You may enter this site";
+// }
+// else{
+//     echo "You must be an adult to enter";
+// }
+
+
+
+$hours = 50;
+$rate = 15;
+$weekly_pay = null;
+
+if($hours <= 0) {
+   $weekly_pay = 0;
+}
+elseif($hours <= 40){
+    $weekly_pay = $hours * $rate;
+}
+else{
+    $weekly_pay = ($rate * 40) + (($hours - 40) * ($rate * 1.5));
+}
+
+echo "You made \${$weekly_pay} this week";
+
+?>
